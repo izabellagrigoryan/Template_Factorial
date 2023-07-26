95b84b3ee1a434cf1c9651fb1d93261b870a9b7d
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-07-26 12:25:16 +0400

    Commit of the compile time Factorial

---
+++ Template_Factorial.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+template <int N> struct Factorial {
+    enum { value = N * Factorial<N - 1>::value };
+};
+
+template <> struct Factorial<0> {
+    enum { value = 1 };
+};
+
+int main() {
+    cout << Factorial<5>::value;
+    return 0;
+}
